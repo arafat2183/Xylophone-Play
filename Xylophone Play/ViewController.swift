a9fd933f0834a15a7311febcbf4bd043335b373f
arafat2183 <akash076987@gmail.com>
2023-07-12 18:15:35 +0900

    first push

---
+++ Xylophone Play/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Xylophone Play
+//
+//  Created by Akash Arafat on 2023/07/11.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+        
+        //reduce the sender's opacity to half
+        sender.alpha = 0.5
+        // This block of code will be execute after 2 seconds delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+            //Bring back the main opacity
+            sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: "Sounds/\(soundName)", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+}
+
